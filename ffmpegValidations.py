632969bf70e529a15f52c32b5db28772ae216d57
Aaron Henriquez <aadavidh@gmail.com>
2024-01-10 21:02:11 -0400

    start allow all methods

--- ffmpegValidations.py
+++ ffmpegValidations.py
@@ -178,7 +178,7 @@ def statusVideo():
     return resp
 
 
-@app.route("/ffmpeg/start/", methods = ["POST", "GET"]
+@app.route("/ffmpeg/start/", methods = ["POST", "GET"])
 def startVideo():
     if request.method == "POST":
         if ffmpegStart(request.form['ffmpeg-command']):
